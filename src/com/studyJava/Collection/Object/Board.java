512e0e288df66328afb11c8e6050328ac10c9344
luna <eskw0701@naver.com>
2022-11-29 19:26:40 +0900

    [collection] Vector , LinkedList 공부

---
+++ src/com/studyJava/Collection/Object/Board.java
@@ -0,0 +1,13 @@
+package com.studyJava.Collection.Object;
+
+public class Board {
+	String subject;
+	String content;
+	String writer;
+	
+	public Board(String sub, String content, String writer) {
+		this.subject = sub;
+		this.content = content;
+		this.writer = writer;
+	}
+}
